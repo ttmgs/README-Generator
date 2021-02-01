9110ab7084f5a5edeada6111cd31b7b788262bae
Magdi Tiea <magditiea@gmail.com>
2021-01-31 21:51:00 -0400

    readme questions added

--- index.js
+++ index.js
@@ -2,3 +2,55 @@ const fs = require("fs");
 const path = require("path");
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
+
+// array of questions for user
+const questions = [
+    {
+    type: "input",
+    name: "Github Username",
+    message: "What is your GitHub username?"
+  },
+  {
+    type: "input",
+    name: "Email",
+    message: "What is your email address?"
+  },
+  {
+    type: "input",
+    name: "Title",
+    message: "What is your project's name?"
+  },
+  {
+    type: "input",
+    name: "Description",
+    message: "Please write a short description of your project"
+  },
+  {
+    type: "list",
+    name: "License",
+    message: "What kind of license should your project have?",
+    choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
+  },
+  {
+    type: "input",
+    name: "Installation",
+    message: "What command should be run to install dependencies?",
+    default: "npm i"
+  },
+  {
+    type: "input",
+    name: "Test",
+    message: "What command should be run to run tests?",
+    default: "npm test"
+  },
+  {
+    type: "input",
+    name: "Usage",
+    message: "What does the user need to know about using the repo?",
+  },
+  {
+    type: "input",
+    name: "Contributing",
+    message: "What does the user need to know about contributing to the repo?",
+  }
+];
